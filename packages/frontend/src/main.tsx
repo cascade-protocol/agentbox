5f3deff6cc3415a4a68f25bd0783e398d675f5a1
tenequm <tenequm@gmail.com>
2026-02-24 00:03:03 +0000

    fix(frontend): use Helius RPC for wallet adapter instead of public endpoint

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,8 +1,10 @@
 import { backpack, createDefaultClient, phantom, solflare } from "@solana/client";
+import type { ClusterUrl } from "@solana/kit";
 import { SolanaProvider } from "@solana/react-hooks";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
 
+import { env } from "./env";
 import { routeTree } from "./routeTree.gen";
 import "./app.css";
 
@@ -22,8 +24,11 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const rpcUrl = `https://mainnet.helius-rpc.com/?api-key=${env.heliusApiKey}` as ClusterUrl;
+
 const client = createDefaultClient({
   cluster: "mainnet-beta",
+  rpc: rpcUrl,
   walletConnectors: [...phantom(), ...solflare(), ...backpack()],
 });
 
